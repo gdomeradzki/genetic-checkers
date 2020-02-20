4d729f15ec5e77ba90d678ad30c823c9536f4d15
Grzegorz Domeradzki <grzegorz@domeradzki.com>
2020-02-20 21:56:47 +0100

    CI now builds all targets

--- run_CI.sh
+++ run_CI.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
 clear_dir(){
+    show_yellow_text "CI dir cleanup..."
     rm -rf build_ci
 }
 build_ci(){
     mkdir build_ci
     cd build_ci
+    show_yellow_text "Build starts..."
     cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DQt5_DIR=$1 ..
+    if [ $? -ne 0 ]; then
+        show_red_text "CMake failed."
+        clear_dir
+        exit 1
+    fi
+    make -j$(nproc)
+    if [ $? -ne 0 ]; then
+        show_red_text "Build failed."
+        clear_dir
+        exit 1
+    fi
+    show_green_text "Build succeed!"
     cd ..
 }
 show_green_text(){
-    echo -e "\e[32m \e[1m $1"
+    echo -e "\e[32m\e[1m$1"
     echo -e "\e[0m"
 }
 show_red_text(){
-    echo -e "\e[31m \e[1m $1"
+    echo -e "\e[31m\e[1m$1"
+    echo -e "\e[0m"
+}
+show_yellow_text(){
+    echo -e "\e[33m\e[1m$1"
     echo -e "\e[0m"
 }
 if [ $# -eq 0 ]; then
@@ -25,13 +43,14 @@ fi
 clear_dir
 build_ci $1
 
+show_yellow_text "Clang tidy starts..."
 ./run-clang-tidy.py -p build_ci/ -header-filter='genetic-checkers/checkers_en*' genetic-checkers/checkers_en* -quiet
 
 if [ $? -ne 0 ]; then
-    show_red_text "Clang tidy failed..."
+    show_red_text "Clang tidy failed."
     clear_dir
     exit 1
 fi
 
-show_green_text "Clang tidy succeed..."
+show_green_text "Clang tidy succeed!"
 clear_dir
